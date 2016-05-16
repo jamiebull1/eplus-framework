c46756ebdc91490e6dc2d3ca238ccf92f5a1d30b
Jamie Bull <jamie.bull@oco-carbon.com>
2016-05-16 18:30:46 +0200

    Move sampler to file

--- client/src/sampler.py
+++ client/src/sampler.py
@@ -7,7 +7,7 @@ Generate samples from an input file.
 """
 from SALib.analyze import sobol
 from SALib.sample import saltelli
-
+from SALib.util import read_param_file
 import numpy as np
 
 
@@ -15,52 +15,12 @@ def evaluate_model(X):
     print zip(problem['names'], X)
     return X
 
-problem = {
-    'num_vars': 13,
-    'names': ['roof_u_value', 'wall_u_value', 'floor_u_value',
-#              'window_u_value', 'window_shgc',
-#              'window2wall', 
-#              'thermal_mass', 
-#              'class_occupancy', 'admin_occupancy',
-              'class_equip_wpm2', 'admin_equip_wpm2', 
-              'class_light_wpm2', 'admin_light_wpm2', 
-#              'schedules', 
-              'heating_setpoint', 
-              'cooling_setpoint',
-              'ventilation', 
-              'infiltration',
-              'boiler_efficiency', 
-#              'cooling_efficiency',
-              'weather_file',
-              ],
-    'bounds': [[0.2, 1.5],  # roof_u_value
-               [0.2, 1.5],  # wall_u_value
-               [0.2, 1.5],  # floor_u_value
-#               [1.5, 4.0],  # window_u_value
-#               [0.2, 0.8],  # window_shgc
-#               [0.2, 0.6],  # wwr
-#               [0, 1],      # thermal_mass
-#               [0.1, 0.5],  # class_occupancy
-#               [0.1, 0.5],  # admin_occupancy
-               [3, 9],      # class_equip_wpm2
-               [9, 18],     # admin_equip_wpm2
-               [5, 12],     # class_light_wpm2
-               [8, 15],     # admin_light_wpm2
-#               [-0.1, 0.1], # schedules
-               [17, 25],    # heating_setpoint
-               [21, 28],    # cooling_setpoint
-               [5, 16],     # ventilation
-               [0.2, 1],    # infiltration
-               [0.7, 0.9],  # boiler_efficiency
-#               [1, 3.5],    # cooling_efficiency
-               [0, 1],      # weather_file
-               ],
-}
+problem = read_param_file('/client/data/parameters.txt')
 
 def samples():
     """Sample parameter values and return a generator of jobs.
     """
-    runs = saltelli.sample(problem, 1, calc_second_order=False)
+    runs = saltelli.sample(problem, 3, calc_second_order=True)
     as_floats = enumerate(zip(problem['names'], (float(n) for n in run)) 
                  for run in runs)
     empty_results = np.nan * np.empty(len(runs))
@@ -68,7 +28,4 @@ def samples():
 
 
 def analyse(Y):
-    Si = sobol.analyze(problem, Y, print_to_console=False, calc_second_order=False)
-    print problem['names']
-    print "Total:", Si['ST'].round(2)
-    print "1st-order:", Si['S1'].round(2)
+    Si = sobol.analyze(problem, Y, print_to_console=True, calc_second_order=True)
