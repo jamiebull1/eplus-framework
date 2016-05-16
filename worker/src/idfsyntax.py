f6158c9c930a011b4f8b99b8a548399fcc5b9298
Jamie Bull <jamie.bull@oco-carbon.com>
2016-05-16 18:31:19 +0200

    Add more parameters

--- worker/src/idfsyntax.py
+++ worker/src/idfsyntax.py
@@ -7,15 +7,50 @@ changed to suit the job in hand.
 """
 import logging
 
+from eppy.modeleditor import IDF
+from eppy.function_helpers import getcoords
+
 logging.basicConfig(level=logging.DEBUG)
 #logging.basicConfig(filename='../var/log/eplus.log', level=logging.DEBUG)
 
-def set_u_value(idf, name, target_u):
-    material = idf.getobject('MATERIAL', name)
-    m = material.Thickness
-    surface_R = 0.18
-    material.Conductivity = m  / ((1 / target_u) - surface_R)
+def set_windows(idf, job):
+    wwr, u_value, shgc = (job['window2wall'], 
+                          job['window_u_value'], 
+                          job['window_shgc'])
+    # set window properties
+    layer = idf.newidfobject(
+        'WINDOWMATERIAL:SIMPLEGLAZINGSYSTEM', 
+        Name = 'WindowGlazing',
+        UFactor = u_value,
+        Solar_Heat_Gain_Coefficient = shgc,        
+        )
+    construction = idf.newidfobject(
+        'CONSTRUCTION',
+        Name='dummy_window',
+        Outside_Layer=layer.Name
+        )
     
+    # get external walls
+    walls = [w for w in idf.idfobjects['BUILDINGSURFACE:DETAILED']
+             if w.Outside_Boundary_Condition.lower() == 'outdoors'
+             and w.Surface_Type.lower() == 'wall']
+    # add windows
+    for wall in walls:
+        logging.debug(wall.Name)
+        pts = window_vertices_given_wall_vertices(vertices(wall), wwr)
+        window = idf.newidfobject('FENESTRATIONSURFACE:DETAILED',
+            Name = wall.Name + " WINDOW",
+            Surface_Type = 'Window',
+            Construction_Name = 'dummy_window',
+            Building_Surface_Name = wall.Name,
+            Number_of_Vertices = len(pts)
+        )
+        for i, v in enumerate(pts):
+            window['Vertex_{0}_Xcoordinate'.format(i+1)] = v[0]
+            window['Vertex_{0}_Ycoordinate'.format(i+1)] = v[1]
+            window['Vertex_{0}_Zcoordinate'.format(i+1)] = v[2]
+        
+
 def prepare_idf(idf, job):
     logging.debug("Editing IDF")
     for key, value in job.items():
@@ -29,7 +64,6 @@ def prepare_idf(idf, job):
     # infiltration and ventilation
     set_infiltration(idf, job)
     set_ventilation(idf, job)
-    
     # fabric U values (add dummy constructions)
     set_u_value(idf, 'wall', job['wall_u_value'])
     set_u_value(idf, 'floor', job['floor_u_value'])
@@ -39,10 +73,10 @@ def prepare_idf(idf, job):
     # set roof thermal mass
     
     # windows
-    # set window U values
-    # set window SHGC
+    set_windows(idf, job)
     
-    # thermal mass
+    # occupancy    
+    set_occupancy(idf, job)
     
     # schedules
     
@@ -51,6 +85,28 @@ def prepare_idf(idf, job):
 
     return idf
 
+def set_occupancy(idf, job):
+    occupants = idf.idfobjects['PEOPLE']
+    for occ in occupants:
+        if occ.Zone_or_ZoneList_Name.lower() == 'admin':
+            occ.People_per_Zone_Floor_Area = job['admin_occupancy']
+        elif occ.Zone_or_ZoneList_Name.lower() == 'class':
+            occ.People_per_Zone_Floor_Area = job['class_occupancy']
+
+
+def set_u_value(idf, name, target_u):
+    material = idf.getobject('MATERIAL', name)
+    m = material.Thickness
+    surface_R = 0.18
+    material.Conductivity = m  / ((1 / target_u) - surface_R)
+ 
+    
+def set_thermal_mass(idf, name, target_u):
+    material = idf.getobject('MATERIAL', name)
+    m = material.Thickness
+    surface_R = 0.18
+    material.Conductivity = m  / ((1 / target_u) - surface_R)
+    
 
 def set_lights(idf, job):
     lights = idf.idfobjects['LIGHTS']
@@ -73,6 +129,8 @@ def set_weather(idf, job):
         idf.epw = './data/cntr_Islington_TRY.epw'
     else:
         idf.epw = './data/2050_Islington_a1b_90_percentile_TRY.epw'
+    logging.debug(job['weather_file'])
+    logging.debug(idf.epw)
 
 def set_infiltration(idf, job):
     for zone in idf.idfobjects['ZONE']:
@@ -191,5 +249,44 @@ def add_or_replace_idfobject(idf, key, aname='', **kwargs):
         idf.removeidfobject(idf.getobject(key, name))
     return idf.newidfobject(key, aname, **kwargs)
 
+def window_vertices_given_wall_vertices(vertices, glazing_ratio):
+    """
+    Calculate window vertices given wall vertices and glazing ratio.
+    For each axis:
+    1) Translate the axis points so that they are centred around zero
+    2) Either:
+        a) Multiply the z dimension by the glazing ratio to shrink it vertically
+        b) Multiply the x or y dimension by 0.995 to keep inside the surface
+    3) Translate the axis points back to their original positions
+    
+    Parameters
+    ----------
+    vertices : list of lists
+        List of [x, y, z] vertices, starting a the top left of a surface and
+        working around it counterclockwise. We expect each surface to have four
+        vertices.
+    
+    Returns
+    -------
+    list 
+        Window vertices bounding a vertical strip midway up the surface.
+    
+    """
+    average_x = sum([x for x, _y, _z in vertices]) / len(vertices)
+    average_y = sum([y for _x, y, _z in vertices]) / len(vertices)
+    average_z = sum([z for _x, _y, z in vertices]) / len(vertices)
+    # move windows in 0.5% from the edges so they can be drawn in SketchUp
+    window_points = [[
+                      ((x - average_x) * 0.995) + average_x,
+                      ((y - average_y) * 0.995) + average_y,
+                      ((z - average_z) * glazing_ratio) + average_z
+                      ]
+                     for x, y, z in vertices]
 
+    return window_points
 
+def vertices(surface):
+    """Get vertices as (x,y,z) tuples.
+    """
+    return getcoords(surface)
+    
