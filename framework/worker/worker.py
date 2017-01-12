5e9449aa009c6ef960a54ca9498f1024f6396923
Jamie Bull <jamie.bull@oco-carbon.com>
2017-01-12 10:24:03 +0100

    Replace worker module with simpler version

--- framework/worker/worker.py
+++ framework/worker/worker.py
@@ -22,19 +22,28 @@ from runner import run as eplus_run
 
 
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
-JOBS_DIR = os.path.join(THIS_DIR, os.pardir, 'jobs')
+JOBS_DIR = os.path.join(THIS_DIR, 'jobs')
+RESULTS_DIR = os.path.join(THIS_DIR, 'results')
 
 
 def get_jobs():
-    """
+    """Pick up the jobs currently in the jobs directory.
+    
+    Returns
+    -------
+    list
+        List of absolute paths to each zipped job.
+
     """
     jobs = [os.path.join(JOBS_DIR, job)
             for job in os.listdir(JOBS_DIR)]
     return jobs
 
 
-def unzip(src, dest=None, rm=False):
+def unzip_dir(src, dest=None, rm=False):
     """Unzip a zipped file.
+    
+    This is used for the incoming jobs.
     
     Parameters
     ----------
@@ -42,7 +51,7 @@ def unzip(src, dest=None, rm=False):
         Path to the zip archive.
     dest : str, optional {default: None}
         The destination folder.
-    rm : bool
+    rm : bool, optional {default: False}
         Flag indicating whether to delete the archive once unzipped.
 
     """
@@ -52,17 +61,37 @@ def unzip(src, dest=None, rm=False):
         os.remove(src)
 
 
+def zip_dir(src, dest, rm=False, extensions=None):
+    """Zip a directory.
+    
+    This is used for zipping results to be collected.
+    
+    src : str
+        Path to the directory to be zipped.
+    dest : str
+        The destination zipped archive name.
+    rm : bool, optional {default: False}
+        Flag indicating whether to delete the source directory once zipped.
+    extensions : list, optional {default: None}
+        List of extensions to keep. If None, keeps all.
+
+    """    
+    pass
+
+
 def main():
     while True:
         jobs = get_jobs()
         run_dir = tempfile.mkdtemp()
         if jobs:
             logging.debug('found %i jobs' % len(jobs))
-            unzip(jobs[0], run_dir)
+            unzip_dir(jobs[0], run_dir)
 
             idf = os.path.join(run_dir, 'in.idf')
             epw = os.path.join(run_dir, 'in.epw')
-            eplus_run(idf, epw, output_directory='../results')
+            eplus_run(idf, epw, output_directory=RESULTS_DIR)
+            # postprocess anything that needs it
+            
     
     
 if __name__ == "__main__":
